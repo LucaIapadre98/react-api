7a1a9ef9a72aa041cfc7268ad9087ceb0c4d889e
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-28 16:28:02 +0200

    Aggiunto useState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,38 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const apiUrlActors= "https://lanciweb.github.io/demo/api/";
 
 export default function Main(){
-  axios
-  .get(`${apiUrlActors}/actors`)
-  .then(res=>{
-    const{ data }= res
-    console.log(data);
+
+  //DICHIARO LO USESTATE PARTENDO DALL'ARRAY VUOTO //
+  const [actorsCharacters, setActorsCharacters] = useState([]);
+
+  //DICHIARO LO USEEFFECT //
+ useEffect(()=>{
+  
+  //CHIAMO AXIOS //
+  axios.get(`${apiUrlActors}/actors`).then(res=>{
+    const{ data }= res;
     
-  })
+    //DESTRUTTURO PER PRENDERE LE CHIAVI CHE MI SERVONO //
+    const charactersChange = data.map((character) => {
+      const { id, name, birth_year, nationality, biography, image} = character;
+      return { id, name, birth_year, nationality, biography, image};
+    });
+
+    setActorsCharacters(charactersChange);
+  });
+ }, []);
 
   return (
     <main>
       <h1>Axios React</h1>
+      <ul>
+        {actorsCharacters.map((character)=>(
+          <li key={character.id}>{character.name}</li>
+        ))}
+      </ul>
     </main>
   );
 }
